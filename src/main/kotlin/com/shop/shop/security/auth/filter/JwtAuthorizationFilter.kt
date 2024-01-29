7e6aa4274e27ad51e00c2ef3e80ec35901dc2075
foko <qkrrudgh0@naver.com>
2024-01-29 16:58:38 +0900

    bug:필터 예외처리 수정
    필터에서 바로 예외를 던지는게 아닌,response에 예외를 담고 다음으로 넘기는걸로 수정

--- src/main/kotlin/com/shop/shop/security/auth/filter/JwtAuthorizationFilter.kt
+++ src/main/kotlin/com/shop/shop/security/auth/filter/JwtAuthorizationFilter.kt
@@ -1,20 +1,22 @@
 package com.shop.shop.security.auth.filter
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
-import com.shop.shop.constants.Constants
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.shop.shop.member.repository.MemberRepository
 import com.shop.shop.security.auth.PrincipalDetails
 import com.shop.shop.security.jwt.JwtUtil
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.web.server.ResponseStatusException
+
 
 //토큰인증시(일반+소셜)토큰확인필터,토큰확인후 세션에 강제로그인
 //유저가 로그인은 이미 했고, 그 이후로 요청이 들어올때마다 작동하는 필터
@@ -28,7 +30,21 @@ class JwtAuthorizationFilter(authManager: AuthenticationManager,
             chain.doFilter(request, response)
             return
         }
-        val username=JwtUtil().getTokenUsername(jwtHeader)
+        println(jwtHeader)
+        var username=""
+        try {
+            username=JwtUtil().getTokenUsername(jwtHeader)
+        } catch (e:Exception){
+            response.status=HttpStatus.UNAUTHORIZED.value()
+            response.contentType=MediaType.APPLICATION_JSON_VALUE
+            response.characterEncoding="UTF-8"
+            val statusException = ResponseStatusException(HttpStatus.UNAUTHORIZED, "토큰이 이상함")
+            ObjectMapper().writeValue(response.writer,statusException)
+            chain.doFilter(request, response)
+            return
+        }
+
+
         println(username)
         val memberEntity = memberRepository.findByUsername(username)?:
             throw UsernameNotFoundException("잘못된 토큰")
