14ad4fcade88c821f03e1ef55b445b7b08b6eb83
foko <qkrrudgh0@naver.com>
2024-01-31 14:14:21 +0900

    feat:일반회원가입시 중복체크 및 api응답형식변경

--- src/main/kotlin/com/shop/shop/member/controller/JoinController.kt
+++ src/main/kotlin/com/shop/shop/member/controller/JoinController.kt
@@ -1,5 +1,7 @@
 package com.shop.shop.member.controller
 
+import com.shop.shop.common.ApiHeader
+import com.shop.shop.common.Result
 import com.shop.shop.member.domain.Member
 import com.shop.shop.member.repository.MemberRepository
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -12,13 +14,16 @@ import org.springframework.web.bind.annotation.RestController
 class JoinController(val memberRepository: MemberRepository,
                      val pwEncoder: BCryptPasswordEncoder) {
     @PostMapping("/join")
-    fun join(@RequestBody member:Member):String{
+    fun join(@RequestBody member:Member):Result<ApiHeader>{
+        if(memberRepository.findByUsername(member.username)!=null){
+            return Result(ApiHeader(400,"중복아이디"))
+        }
         member.role="ROLE_USER"
         val rawPassword=member.password
         val encPassword=pwEncoder.encode(rawPassword)
         member.password=encPassword
         memberRepository.save(member)
 
-        return "가입성공"
+        return Result(ApiHeader())
     }
 }
