dfe59d3ede60983058f068e7633e19f17a3eb1e7
foko <qkrrudgh0@naver.com>
2024-02-07 14:27:40 +0900

    0207

---
+++ src/main/kotlin/com/shop/shop/post/domain/Post.kt
@@ -0,0 +1,23 @@
+package com.shop.shop.post.domain
+
+import com.shop.shop.domain.BaseEntity
+import jakarta.persistence.*
+
+//사진,제목,가격,할인율,무료배송,구매수,본문(json이나 text,사진은 텍스트중간에 경로로),쿠폰으로 구성됨
+@Entity
+class Post(
+    var title:String,
+    var price:Int,
+    var discountRate:Int,
+    var shipCount:Int,
+    @Column(columnDefinition = "TEXT")
+    var body:String,
+    var coupon:String="",
+    var buyCount:Int=0,
+    var titleImage:String="",
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id:Long=0,
+):BaseEntity() {
+
+}
---
+++ src/main/kotlin/com/shop/shop/post/repository/PostRepository.kt
@@ -0,0 +1,15 @@
+package com.shop.shop.post.repository
+
+import com.shop.shop.post.domain.Post
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+interface PostRepository:JpaRepository<Post,Long> {
+
+    //fun findByTitleContaining(title:String,pageable: Pageable): Page<Post>
+    @Query("select p from Post p where p.title like %:title%") //전문검색변경필요
+    fun findByTitleContaining(@Param("title") title:String,pageable: Pageable): Page<Post>
+}
