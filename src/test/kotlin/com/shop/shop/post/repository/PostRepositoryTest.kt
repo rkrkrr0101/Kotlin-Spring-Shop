f8f4eb27af446ba5a08a50b94c607af2bcd5ef96
foko <qkrrudgh0@naver.com>
2024-02-13 00:40:32 +0900

    test:필요한곳에서만 테스트컨테이너 가져다쓰게 변경

--- src/test/kotlin/com/shop/shop/post/repository/PostRepositoryTest.kt
+++ src/test/kotlin/com/shop/shop/post/repository/PostRepositoryTest.kt
@@ -3,6 +3,7 @@ package com.shop.shop.post.repository
 import com.shop.shop.member.domain.Member
 import com.shop.shop.member.repository.MemberRepository
 import com.shop.shop.post.domain.Post
+import jakarta.transaction.Transactional
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
@@ -14,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.jdbc.Sql
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.junit.jupiter.Container
@@ -22,6 +24,7 @@ import javax.sql.DataSource
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = [ "spring.config.location = classpath:application-testContainer.yml"])
 @Testcontainers
 //@Sql(scripts = ["classpath:/init_data/fulltext_index_create.sql"])
 class PostRepositoryTest(@Autowired val postRepository: PostRepository,
@@ -40,15 +43,15 @@ class PostRepositoryTest(@Autowired val postRepository: PostRepository,
 
     @Test
     fun abc(){
+        val jdbcTemplate = JdbcTemplate(dataSource)
+        jdbcTemplate.execute("alter table post add fulltext key fx_title (title) with parser ngram;")
+
         val member = Member("username111", "password111", "name111", "qqq@aqw.com")
         val post = Post("aaeqeq", 123, 10, 1, "da", "none", 3, "aa.img")
         memberRepository.save(member)
         val findMember = memberRepository.findByUsername("username111")!!
         post.member=findMember
         postRepository.save(post)
-
-        val jdbcTemplate = JdbcTemplate(dataSource)
-        jdbcTemplate.execute("alter table post add fulltext key fx_title (title) with parser ngram;")
 
         val pageRequest= PageRequest.of(
             0,10, Sort.by("price").descending()
